02e23bae862d7339b20aa3252359480fe38690ce
Icaro <icarolavrador@gmail.com>
2024-03-18 20:53:09 +0000

    Add more scripts to make life easier

---
+++ SmartContracts/Scripts/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+eosio-cpp -I include -o build/p2ewgamelogi.wasm src/p2ewgamelogi.cpp --abigen
---
+++ SmartContracts/Scripts/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cleos set contract p2ewgamelogi . p2ewgamelogi.wasm --abi p2ewgamelogi.abi -p p2ewgamelogi@active
