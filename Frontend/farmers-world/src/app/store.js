9ec4a933339018c458fe5ec22f556bffd6816efd
Icaro <icarolavrador@gmail.com>
2024-07-04 12:25:07 +1200

    Add redux dev tools

--- Frontend/farmers-world/src/app/store.js
+++ Frontend/farmers-world/src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+
 const store = configureStore({
     reducer:{
         auth: authReducer
-    }
+    },
+    devTools: process.env.NODE_ENV != 'production'
 })
 
 export default store;
